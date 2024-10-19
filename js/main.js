cd86f9bc7e53c1affb99d1f73f7fe6b90f03c978
Carlo Elvisi <carloelvisi01@outlook.it>
2024-10-19 13:34:02 +0200

    fix errors

--- js/main.js
+++ js/main.js
@@ -51,46 +51,42 @@ Domande da  farsi quando si crea una funzione:
 2. Ho bisogno di parametri?
 3. Devo restituire un valore?
 4. Se sì, di che tipo? */
-
 function askNumber(min, max, message = `Dammi un numero`) {
   let userNumber = parseInt(prompt(message));
   while (isNaN(userNumber) || userNumber < min || userNumber > max) {
-    userNumber = parseInt(prompt(`Valore non valido per favore ` + message));
+      userNumber = parseInt(prompt(`Valore non valido, per favore ${message}`));
   }
   return userNumber;
 }
+
 function askString(whitelist = [], message = `Dammi un testo`) {
   let userString = prompt(message);
 
   if (whitelist.length > 0) {
-    while (!whitelist.includes(userString)) {
-      userString = prompt(
-        `la parola deve essere inculsa nella whitelist` + whitelist
-      );
-    }
+      while (!whitelist.includes(userString)) {
+          userString = prompt(`La parola deve essere inclusa nella whitelist: ${whitelist.join(', ')}`);
+      }
   }
   return userString;
 }
+
 function isNumberEven(num) {
-  if (num % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return num % 2 === 0;
 }
 
 function generateNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function isUserWinner(userNumber, opponentNumber, userChoice) {
   const sum = userNumber + opponentNumber;
-  console.log(`userwon` + userWon);
-  return (
-    (userChoice === "pari" && isNumberEven()) ||
-    (userChoice === "dispari" && !isNumberEven())
-  );
+  const userWon = (userChoice === "pari" && isNumberEven(sum)) ||
+                  (userChoice === "dispari" && !isNumberEven(sum));
+  console.log(`User won: ${userWon}`);
+  return userWon;
 }
-const userNumber = askNumber(1, 5, `Dammi un numero da 1 a 5 `);
-const userChoice = askString([`pari,dispari`], `Dammi pari o dispari`);
+
+const userNumber = askNumber(1, 5, `Dammi un numero da 1 a 5`);
+const userChoice = askString(['pari', 'dispari'], `Dammi pari o dispari`);
 const pcNumber = generateNumber(1, 5);
 const userWon = isUserWinner(userNumber, pcNumber, userChoice);
